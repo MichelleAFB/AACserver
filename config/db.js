21a05c00fa3f55b458d466b111393219eb3ddfc0
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2022-11-30 14:44:08 -0600

    switch back to local database
    maybe database is the problem.stopped using clearDB

--- config/db.js
+++ config/db.js
@@ -2,12 +2,19 @@
 
 const mysql=require('mysql')
 
-
+/*
 const db_config={
   host:'us-cdbr-east-06.cleardb.net',
   user:'b9a25f42e722fd',
   password:'0331ec28',
   database:'heroku_74ccdb90d27e5ae'
+}
+*/
+const db_config={
+  host:'localhost',
+  user:'root',
+  password:'',
+  database:'aacdb'
 }
 
 /*
