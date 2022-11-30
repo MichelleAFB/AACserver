fa216b7bf18f4e63a9a91ffccc339534d8ed2cde
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2022-11-30 14:51:57 -0600

    changed back to clearDb
    changed back to cleardb local db does not work

--- config/db.js
+++ config/db.js
@@ -2,14 +2,14 @@
 
 const mysql=require('mysql')
 
-/*
+
 const db_config={
   host:'us-cdbr-east-06.cleardb.net',
   user:'b9a25f42e722fd',
   password:'0331ec28',
   database:'heroku_74ccdb90d27e5ae'
 }
-*/
+/*
 const db_config={
   host:'localhost',
   user:'root',
@@ -17,7 +17,7 @@ const db_config={
   database:'aacdb'
 }
 
-/*
+
 mysql://b9a25f42e722fd:0331ec28@us-cdbr-east-06.cleardb.net/heroku_74ccdb90d27e5ae?reconnect=true
 */
 
